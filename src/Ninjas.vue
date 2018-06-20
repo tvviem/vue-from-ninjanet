29628ae8b702b62dd30a340d8ae87b23a525d990
viem trieu <trieuvinhviem@gmail.com>
2018-06-20 10:13:53 +0700

    Webpack-Simple Create new components and import global or locally

---
+++ src/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+      <li v-for="(item, index) in ninjas" :key="index"> {{ item }} </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ninjas: ['yoshi', 'tvviem', 'ttxuyen']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* // make ninjas component global, and ninjas component can use in other component
+import Ninjas from './Ninjas.vue'
+Vue.component('ninjas', Ninjas); */
+
 new Vue({
   el: '#app',
   render: h => h(App)
